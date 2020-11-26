e4f65dd16c1b5728772bb7291b5f194df067e4f8
Mikalai Shalupin <Mikalai_Shalupin@epam.com>
2020-11-26 11:18:38 +0300

    add search-results-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { SearchResultsIemComponent } from './components/search-results-iem/search-results-iem.component';
+import { SearchResultsItemComponent } from './components/search-results-item/search-results-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchResultsComponent
+    SearchResultsComponent,
+    SearchResultsIemComponent,
+    SearchResultsItemComponent
   ],
   imports: [
     BrowserModule
