17d2335693cc3f7442f939cc0be90a56b2086622
Mikalai Shalupin <Mikalai_Shalupin@epam.com>
2020-11-26 11:19:53 +0300

    correct app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { SearchResultsIemComponent } from './components/search-results-iem/search-results-iem.component';
 import { SearchResultsItemComponent } from './components/search-results-item/search-results-item.component';
 
 @NgModule({
@@ -12,7 +11,6 @@ import { SearchResultsItemComponent } from './components/search-results-item/sea
     AppComponent,
     HeaderComponent,
     SearchResultsComponent,
-    SearchResultsIemComponent,
     SearchResultsItemComponent
   ],
   imports: [
