bdfa24009d279d8ba969d60258e416b2bdbfe04f
Mikalai Shalupin <Mikalai_Shalupin@epam.com>
2020-11-27 13:01:41 +0300

    add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { SearchResultsItemComponent } from './components/search-results-item/search-results-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SearchResultsItemComponent } from './components/search-results-item/sea
     SearchResultsItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
